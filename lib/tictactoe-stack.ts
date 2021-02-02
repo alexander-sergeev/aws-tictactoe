a7df8afc36a57b2ea1d8b5985dac25fa90c3dd82
Alexander <75954795+alexander-sergeev@users.noreply.github.com>
2021-02-02 18:11:30 +0200

    Add hello world lambda

--- lib/tictactoe-stack.ts
+++ lib/tictactoe-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import { BlockPublicAccess, Bucket } from '@aws-cdk/aws-s3';
 import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 import { CloudFrontWebDistribution, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class TictactoeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -31,6 +32,12 @@ export class TictactoeStack extends cdk.Stack {
       sources: [ s3deploy.Source.asset('./client') ],
       destinationBucket: bucket,
       distribution,
+    });
+
+    const helloWorldLambda = new lambda.Function(this, 'HelloWorldHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
     });
   }
 }
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+async function handler () {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: 'Hello world',
+  };
+}
+
+export { handler };
